a81140b8312d5ca361b35f873d2f425d8a168fe8
Lance Tran <thelancetran@gmail.com>
2024-05-06 00:17:29 -0700

    added unit testing

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Lance Tran
 https://thelancetran.github.io/Lab5_Starter/expose.html
+
+### Explore Part 3
+1. I would not use a unit test to test the "message" feature of a messaging application because this feature seems too large scale. Assuming the "message" feature allows a user to write and send a message to another user, I believe that this feature has too many factors to take into account. For example, some of these factors may include network communication, server infrastructure, external services, APIs, etc. 
+2. I would use a unit test to test the "max message length" feature of a messaging application because this feature seems to be on a smaller scale. Assuming this feature prevents the user from typing more than 80 characters, I believe that this feature would be easier to test by tracking the number of characters the user has typed. Additionally, this feature can be tested locally without many external factors to affect it. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,50 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+test('PHONE NUMBER no parentheses', () => {
+  expect(isPhoneNumber(`123-456-7890`)).toBe(true);
+});
+
+test('PHONE NUMBER parentheses', () => {
+  expect(isPhoneNumber(`(123) 456-7890`)).toBe(true);
+});
+
+test('PHONE NUMBER letters', () => {
+  expect(isPhoneNumber(`(123) 456-test`)).toBe(false);
+});
+
+test('PHONE NUMBER no hyphen', () => {
+  expect(isPhoneNumber(`(123) 456 7890`)).toBe(false);
+});
+
+test('EMAIL regular', () => {
+  expect(isEmail(`example@gmail.com`)).toBe(true);
+});
+
+test('EMAIL underscore', () => {
+  expect(isEmail(`example_second@hello.io`)).toBe(true);
+});
+
+test('EMAIL spaces', () => {
+  expect(isEmail(`example second@gmail.com`)).toBe(false);
+});
+
+test('EMAIL no domain', () => {
+  expect(isEmail(`example@.com`)).toBe(false);
+});
+
+test('STRONG PW uppercase', () => {
+  expect(isStrongPassword(`Testing`)).toBe(true);
+});
+
+test('STRONG PW numbers', () => {
+  expect(isStrongPassword(`Testing123`)).toBe(true);
+});
+
+test('STRONG PW very long', () => {
+  expect(isStrongPassword(`this is a very long sentence`)).toBe(false);
+});
+
+test('STRONG PW short', () => {
+  expect(isStrongPassword(`h`)).toBe(false);
+});
